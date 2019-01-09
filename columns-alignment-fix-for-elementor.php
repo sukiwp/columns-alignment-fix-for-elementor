f7f2b4b27eef7f96c4eabbcc8e478ccc71683d96
David Rozando <david.rozando@gmail.com>
2019-01-09 11:51:47 +0700

    Change author info

--- columns-alignment-fix-for-elementor.php
+++ columns-alignment-fix-for-elementor.php
@@ -4,8 +4,8 @@ Plugin Name: Columns Alignment Fix for Elementor
 Plugin URI: http://wordpress.org/plugins/columns-alignment-fix-for-elementor
 Description: Add an option to fix Elementor's inconsistent columns alignment when using columns gap on sections.
 Version: 1.1.0
-Author: SukiWP
-Author URI: https://sukiwp.com/about/
+Author: Suki WordPress Theme
+Author URI: https://sukiwp.com/#about
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: columns-alignment-fix-for-elementor
@@ -61,7 +61,7 @@ class Columns_Alignment_Fix_For_Elementor {
 		$element->add_control(
 			'fix_columns_alignment',
 			array(
-				'type' => \Elementor\Controls_Manager::SWITCHER,
+				'type' => Elementor\Controls_Manager::SWITCHER,
 				'label' => esc_html__( 'Enable Columns Alignment Fix', 'columns-alignment-fix-for-elementor' ),
 				'description' => esc_html__( 'It will remove the "weird" columns gap added by Elementor on the left and right side of each section (when `Columns Gap` is active). This helps you to have consistent content width without having to manually readjust it everytime you create sections with `Columns Gap`', 'columns-alignment-fix-for-elementor' ),
 				'return_value' => 'enabled',
@@ -75,20 +75,20 @@ class Columns_Alignment_Fix_For_Elementor {
 	 * Enqueue custom CSS for columns alignment fix.
 	 */
 	public function add_css() {
-		$body = get_option( 'elementor_stretched_section_container', 'body' );
-		if ( empty( $body ) ) {
-			$body = 'body';
+		$container = get_option( 'elementor_stretched_section_container', 'body' );
+		if ( empty( $container ) ) {
+			$container = 'body';
 		}
 
 		$css =
-$body . ' { overflow-x: hidden; }
+$container . ' { overflow-x: hidden; }
 .elementor-section.elementor-columns-alignment-fix-enabled > .elementor-column-gap-default > .elementor-row { width: calc(100% + 20px); margin-left: -10px; margin-right: -10px; }
 .elementor-section.elementor-columns-alignment-fix-enabled > .elementor-column-gap-narrow > .elementor-row { width: calc(100% + 10px); margin-left: -5px; margin-right: -5px; }
 .elementor-section.elementor-columns-alignment-fix-enabled > .elementor-column-gap-extended > .elementor-row { width: calc(100% + 30px); margin-left: -15px; margin-right: -15px; }
 .elementor-section.elementor-columns-alignment-fix-enabled > .elementor-column-gap-wide > .elementor-row { width: calc(100% + 40px); margin-left: -20px; margin-right: -20px; }
 .elementor-section.elementor-columns-alignment-fix-enabled > .elementor-column-gap-wider > .elementor-row { width: calc(100% + 60px); margin-left: -30px; margin-right: -30px; }'
 ;
-		wp_add_inline_style( 'elementor-frontend', esc_attr( $css ) );
+		wp_add_inline_style( 'elementor-frontend', $css );
 	}
 }
 
